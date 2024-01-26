85408f72fdcafca9b0c5bec92e3bdebd8769af4c
souat-sadi-khan <sadik.cse15@gmail.com>
2024-01-27 00:38:48 +0600

    Add about me and portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,33 +86,3 @@ $(document).ready(function () {
   // var containerEl = document.querySelector('.auction-filter');
   // var mixer = mixitup(containerEl);
 });
-
-// const HOURHAND = document.querySelector("#hour");
-// const MINUTEHAND = document.querySelector("#minute");
-// const SECONDHAND = document.querySelector("#second");
-
-// var date = new Date();
-// let hr = date.getHours();
-// let min = date.getMinutes();
-// let sec = date.getSeconds();
-
-// let hrPosition = (hr*360/12) + (min*(360/60)/12);
-// let minPosition = (min*360/60 )+ (sec*(360/60)/60);
-// let secPosition = sec*360/60;
-
-// function runThetime() {
-//   hrPosition = hrPosition+(3/360);
-//   minPosition = minPosition+(6/60);
-//   secPosition = secPosition+(6);
-
-//   HOURHAND.style.transform = "rotate(" + hrPosition + "deg)";
-//   MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)";
-//   SECONDHAND.style.transform = "rotate(" + secPosition + "deg)";
-// }
-
-// var interval = setInterval(runThetime, 1000);
-
-// var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-// var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//   return new bootstrap.Tooltip(tooltipTriggerEl)
-// })  
